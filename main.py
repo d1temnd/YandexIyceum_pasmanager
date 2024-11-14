ceef762988fa4afe0f38efabda4f9dbe3f99442c
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-11-14 22:33:05 +0700

    add logick switch windows

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import sys
 
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from ui.first import Ui_FirstTimeWindow
+from ui.interf import Ui_MainWindow
+from ui.next import Ui_SubsequentWindow
+from backend.qwery import check_table_exists, create_table
+
+testBool = False
 
 
 # Наследуемся от виджета из PyQt6.QtWidgets и от класса с интерфейсом
-class MyWidget(QMainWindow, Ui_FirstTimeWindow):
+class RegUser(QMainWindow, Ui_FirstTimeWindow):
     def __init__(self):
         super().__init__()
         # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
@@ -18,12 +23,29 @@ class MyWidget(QMainWindow, Ui_FirstTimeWindow):
     #     self.label.setText("OK")
 
 
+class MainWin(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+class LoginWin(QMainWindow, Ui_SubsequentWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
 def main():
     app = QApplication(sys.argv)
+    # print(check_table_exists('users.db', 'passMGR'))
+    if check_table_exists('users.db', 'passMGR'):
+        auth = LoginWin()
+        auth.show()
+    else:
+        create_table('users.db')
+        reg_user = RegUser()
+        reg_user.show()
 
-    ex = MyWidget()
-    ex.show()
-    if ex.close(): sys.exit()
     sys.exit(app.exec())
 
 
