6b8e7de16c1d00c8fb48d7b90a78f553926c43b2
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-11-14 17:19:33 +0700

    add tables func

--- backend/qwery.py
+++ backend/qwery.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def check_table_exists(db_name, table_name):
+def check_table_exists(db_name: str, table_name: str) -> bool:
     try:
         # Подключаемся к базе данных
         conn = sqlite3.connect(db_name)
@@ -12,6 +12,7 @@ def check_table_exists(db_name, table_name):
 
         # Получаем результат
         table_exists = cursor.fetchone() is not None
+        print(table_exists)
 
         # Закрываем соединение
         conn.close()
@@ -22,3 +23,42 @@ def check_table_exists(db_name, table_name):
         return False
 
 
+def create_table(db_name):
+    try:
+        # Подключаемся к базе данных
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        # Создаем таблицу users
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT
+            );
+        """)
+
+        # Создаем таблицу user_data
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS passMGR (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                name TEXT NOT NULL,
+                site_url TEXT NOT NULL,
+                login TEXT NOT NULL,
+                password TEXT NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users(user_id)
+            );
+        """)
+
+        # Сохраняем изменения
+        conn.commit()
+        print("Таблицы созданы успешно.")
+
+    except sqlite3.Error as e:
+        print(f"Ошибка при создании таблиц: {e}")
+    finally:
+        # Закрываем соединение
+        conn.close()
+
+
+# create_table("users.db")
+# def add_db(name_db: str, name_table: str, colum: str, data: str) -> None:
