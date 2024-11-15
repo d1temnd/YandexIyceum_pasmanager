cf488ec2c352ff98476e5e550b76a41e13179c67
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-11-16 00:20:16 +0700

    bot send auth cod

---
+++ backend/bot.py
@@ -0,0 +1,31 @@
+import os
+
+import requests
+import random
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+
+
+def gev_auth_cod():
+    cod = ''.join(str(random.randint(1, 9)) for _ in range(5))
+    return cod
+
+
+def sand_code(user_id: int) -> str:
+    login_code = gev_auth_cod()
+
+    response = requests.post(
+        f"https://api.telegram.org/bot{os.getenv('TOKEN')}/sendMessage",
+        params={
+            "chat_id": user_id,
+            "text": login_code
+        }
+    )
+
+    print(response.status_code)
+    return str(login_code)
+
+
+# print(sand_code(996027511))
