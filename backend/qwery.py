5157d5d2310606af7d5f8e6fea0ac26bb8e7c989
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-11-16 16:10:56 +0700

    add func add user

--- backend/qwery.py
+++ backend/qwery.py
@@ -58,7 +58,6 @@ def create_table(db_name):
     except sqlite3.Error as e:
         print(f"Ошибка при создании таблиц: {e}")
     finally:
-        # Закрываем соединение
         conn.close()
 
 
@@ -84,10 +83,36 @@ def check_user(db_name, Nic_name):
         print(f"Ошибка работы с базой данных: {e}")
         return False, None
     finally:
-        if conn:
-            conn.close()
+        conn.close()
 
 
 # print(check_user('../users.db', 'd1temnd'))
+
+
+def create_user(db_name, Nic_name, user_id):
+
+    if check_user(db_name, Nic_name)[0]:
+        return False
+    else:
+        try:
+            conn = sqlite3.connect(db_name)
+            cursor = conn.cursor()
+
+            # Проверяем, существует ли пользователь и получаем user_id
+            cursor.execute("""
+                INSERT INTO users (user_id, user_nicname) VALUES (?, ?);
+            """, (int(user_id), str(Nic_name), ))
+            conn.commit()
+            return True
+
+        except sqlite3.Error as e:
+            print(f"Ошибка работы с базой данных: {e}")
+            return False
+        finally:
+            conn.close()
+
+
+# print(create_user('../users.db', '3', "123123123"))
+
 
 
